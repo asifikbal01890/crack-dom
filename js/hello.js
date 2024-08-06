fe7ec1950f4905e990767ae7d23553181ead1906
asifikbal01890 <asifikbal01890@gmail.com>
2024-08-06 16:45:46 +0600

    first commit

---
+++ js/hello.js
@@ -0,0 +1,12 @@
+console.log('new one');
+
+const a = {
+    name: {
+        name1: 'asif1',
+        Name2: 'asif23',
+        oneName3: 'asif4'
+    },
+    age: 40,
+    gender: 'male'
+};
+console.log(a.name.name1);
